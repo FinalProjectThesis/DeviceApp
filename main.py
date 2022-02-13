73478f2d4f74d8fcbb0715a1a0b42a7d8fab6c9e
RSarona <saronaraulen@gmail.com>
2022-02-13 15:58:12 +0800

    Initialized .gitignore, main_app.kv, main.py
    Added important imports in main.py
    
    Added test buttons on main_app.kv

---
+++ main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty, Clock
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.button import Button
+from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
+from kivy.graphics.context_instructions import Color
+
+class main_app(App):
+    pass
+
+class login_screen(BoxLayout):
+    pass
+
+class signup_screen(BoxLayout):
+    pass
+
+main_app().run()
