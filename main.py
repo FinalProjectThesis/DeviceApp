18860e447891b477a19e60b914e379773be56cda
Joash Diaz <johnjoashcolangodiaz@gmail.com>
2022-02-14 01:43:27 +0800

    Addedurlandhttp requestsparams lookright but wrong

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import urllib3
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.metrics import dp
@@ -12,8 +13,12 @@ from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
 from kivy.graphics.context_instructions import Color
 
 #for the HTTP 
+from kivy.network.urlrequest import UrlRequest
+import urllib
 import requests
 import json
+
+#Asyncio
 import asyncio
 
 #the URL 
@@ -26,10 +31,15 @@ class login_screen(BoxLayout):
     username = StringProperty('')
     password = StringProperty('')
 
+    
+    
+    
     def btn_login_function(self):
+
+        
+
         self.username = self.ids.user_input.text
         self.password = self.ids.pass_input.text
-
         if len(str(self.username)) == 0 or len(str(self.password)) == 0:
             print ("both fields are empty")
             if len(str(self.username)) == 0:
@@ -44,6 +54,16 @@ class login_screen(BoxLayout):
             Username = str(self.username)
             Password = str(self.password)
             print("Username : " + Username + "\n" + "Password : " + Password)
+            
+            # On Success Message of below's request 
+            def loginrequestbody(self,*args):
+                print(params)
+                print("Result is " + str(Loginrequest.result))
+
+            # requests through KIVY's own thing
+            params =({"username":Username,"password":Password})
+            print(params)
+            Loginrequest = UrlRequest('https://uslsthesisapi.herokuapp.com/login', on_success= loginrequestbody, req_body=str(params))
 
 class signup_screen(BoxLayout):
     pass
