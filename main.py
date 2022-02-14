60b864cf8db10c8e58158597f7aa4f7372a6c616
Joash Diaz <johnjoashcolangodiaz@gmail.com>
2022-02-14 12:41:50 +0800

    added logic to SignupScreen

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class LoginScreen(Screen):
         self.username = self.ids.user_input.text
         self.password = self.ids.pass_input.text
         if len(str(self.username)) == 0 or len(str(self.password)) == 0:
-            print ("both fields are empty")
+            print ("both fields are empty") # for debugging
             if len(str(self.username)) == 0:
                 print("please enter a username") # print this at the error label
             else:
@@ -48,7 +48,6 @@ class LoginScreen(Screen):
             # On Success Message of below's request 
             def loginrequestbody(self,*args):
                 print(params)
-                
                 print("Result is " + str(Loginrequest.result))
 
         # test
@@ -59,7 +58,52 @@ class LoginScreen(Screen):
             Loginrequest = UrlRequest('https://uslsthesisapi.herokuapp.com/login', on_success= loginrequestbody, req_body=params,req_headers=headers)
 
 class SignupScreen(Screen):
-    pass
+    username = StringProperty('')
+    password = StringProperty('')
+    confirm_password = StringProperty('')
+    first_name = StringProperty('')
+    last_name = StringProperty('')
+
+    def btn_register_function(self):
+
+        if len(str(self.username)) == 0 or len(str(self.password)) == 0:
+            print ("both fields are empty") # for debugging
+            if len(str(self.username)) == 0:
+                print("please enter a username") # print this at the username error label
+            else:
+                print("username is full") # erase username error label text 
+            if len(str(self.password)) == 0:
+                print("please enter a password") # print this at the password error label
+            else:
+                print ("password is full") # erase error label text
+            if self.password != self.confirm_password:
+                print("Please Confirm your password") # print this at the confirm_password  error label
+            if len(str(self.first_name)) == 0:
+                print("please enter your first name")
+            else:
+                print("firstname is full") # erase first name error label text
+            if  len(str(self.last_name)) ==0:
+                print("please enter your last name ") # print this at the last name  error label
+            else:
+                print("lastname is full") # erase last name error label text
+        else:  # if all checks pass 
+            Username = str(self.username)
+            Password = str(self.password)
+            First_name = str(self.first_name)
+            Last_name = str(self.last_name)
+            
+            params =json.dumps({"username":Username,"password":Password, "first_name": First_name, "last_name": Last_name})
+            print (params)
+            headers= {'Content-type':'application/json','Accept':'text/plain'}
+            Loginrequest = UrlRequest('https://uslsthesisapi.herokuapp.com/register', on_success= loginrequestbody, req_body=params,req_headers=headers)
+
+            
+            # On Success Message of below's request 
+            def loginrequestbody(self,*args):
+                print("Register Success")
+                print(params)
+                print("Result is " + str(Loginrequest.result))
+
 class MainApp(App):
     def build(self):
         return Builder.load_file('main_app.kv')
