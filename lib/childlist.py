9a384279fe7ac355427e6d54ece3d347f2a19b3f
RSarona <saronaraulen@gmail.com>
2022-02-15 20:39:40 +0800

    Added test buttons for childlist, with functioning scrollview

--- lib/childlist.py
+++ lib/childlist.py
@@ -1,10 +1,22 @@
 from lib2to3.pgen2 import token
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang.builder import Builder
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.metrics import dp
 from kivy.properties import StringProperty
 
 Builder.load_file('lib/childlist.kv')
 
 class ChildListScreen(Screen):
-    def test(self):
-        print(self.manager.token)
+    pass
+
+class StackTest(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        for i in range(0, 100):
+            # size = dp(100) + i * 10
+            size = dp(100)
+            b = Button(text=str(i+1), size_hint=(None, 1), width = size)
+            self.add_widget(b)
