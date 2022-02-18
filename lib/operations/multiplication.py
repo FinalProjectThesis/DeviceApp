734d99f86afebdd8e5a94c6dc8220fd18a8f0c01
RSarona <saronaraulen@gmail.com>
2022-02-18 21:55:37 +0800

    Added operation screens (Addition, subtract, division, multi)

---
+++ lib/operations/multiplication.py
@@ -0,0 +1,12 @@
+from kivy.lang import Builder
+from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty, ObjectProperty,Clock
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
+from kivy.graphics.context_instructions import Color
+
+Builder.load_file('lib/kv/operations_kv/multiplication.kv')
+
+class MultiplicationScreen(Screen):
+    pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import imp
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -8,16 +9,25 @@ from lib.login import LoginScreen
 from lib.signup import SignupScreen
 from lib.childlist import ChildListScreen
 from lib.menu import MenuScreen
+from lib.operations.addition import AdditionScreen
+from lib.operations.division import DivisionScreen
+from lib.operations.multiplication import MultiplicationScreen
+from lib.operations.subtraction import SubtractionScreen
 
 class WindowManager(ScreenManager):
     token = StringProperty('')
     parent_username = StringProperty('')
+    student_id = StringProperty('')
 
 sm = ScreenManager()
 sm.add_widget(LoginScreen(name = 'login'))
 sm.add_widget(SignupScreen(name = 'signup'))
 sm.add_widget(ChildListScreen(name = 'childlist'))
 sm.add_widget(MenuScreen(name = 'menu'))
+sm.add_widget(AdditionScreen(name = 'addition'))
+sm.add_widget(DivisionScreen(name = 'division'))
+sm.add_widget(MultiplicationScreen(name = 'multiplication'))
+sm.add_widget(SubtractionScreen(name = 'subtraction'))
 
 class MainApp(App):
     def build(self):
