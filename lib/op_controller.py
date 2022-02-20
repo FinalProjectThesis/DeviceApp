ec0e54cc7ba4e81e1947518fd29296d7aaa0afd2
RSarona <saronaraulen@gmail.com>
2022-02-20 20:45:40 +0800

    Added functionalities in all of addition's difficulty

--- lib/op_controller.py
+++ lib/op_controller.py
@@ -1,7 +1,7 @@
 import random
 from kivy.lang import Builder
 from kivy.metrics import dp
-from kivy.properties import StringProperty, BooleanProperty, ObjectProperty, Clock
+from kivy.properties import StringProperty, BooleanProperty, ObjectProperty, NumericProperty, Clock
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
@@ -18,21 +18,31 @@ class AdditionScreen(Screen):
     counter = 1
 
     def on_pre_enter(self, *args):
-        self.ids.add_label.text = DifficultyScreen.difficulty
-        if DifficultyScreen.difficulty == 'easy':
-            self.load_easy()
-        elif DifficultyScreen.difficulty == 'medium':
-            pass
-        elif DifficultyScreen.difficulty == 'hard':
-            pass
+        # self.ids.add_label.text = DifficultyScreen.difficulty
+        # reset inputs
+        self.reset_inputs()
+
+        # evaluate
+        if self.counter > 10:
+            print('counter is '+ str(self.counter))
+            self.manager.current = 'result'
+            AdditionScreen.score = 0
+            self.counter = 1
+        else:
+            # conditions will load the answers
+            if DifficultyScreen.difficulty == 'easy':
+                self.load_easy()
+            elif DifficultyScreen.difficulty == 'medium':
+                self.load_medium()
+            elif DifficultyScreen.difficulty == 'hard':
+                self.load_hard()
         
-        # conditions will load the answers
         return super().on_pre_enter(*args)
     
     def load_easy(self):
         val1 = random.randint(1, 10)
         val2 = random.randint(1, 10)
-        self.Sum = val1 + val2
+        AdditionScreen.Sum = val1 + val2
         # generate labels
         self.ids.qcount_label.text = str('Q #' + str(self.counter))
         self.ids.add_label.text = str(val1) + ' + ' + str(val2)
@@ -41,49 +51,66 @@ class AdditionScreen(Screen):
     def load_medium(self):
         val1 = random.randint(10, 100)
         val2 = random.randint(10, 100)
-        self.Sum = val1 + val2
+        AdditionScreen.Sum = val1 + val2
         # generate labels
-        self.ids.qcount_label.text = str('Q #' + self.counter)
-        # self.ids.add_label.text = str(val1) + ' + ' + str(val2)
+        self.ids.qcount_label.text = str('Q #' + str(self.counter))
+        self.ids.add_label.text = str(val1) + ' + ' + str(val2)
+        print(str(self.counter))
     
     def load_hard(self):
         val1 = random.randint(100, 1000)
         val2 = random.randint(100, 1000)
-        self.Sum = val1 + val2
+        AdditionScreen.Sum = val1 + val2
         # generate labels
-        self.ids.qcount_label.text = str('Q #' + self.counter)
-        # self.ids.add_label.text = str(val1) + ' + ' + str(val2)
+        self.ids.qcount_label.text = str('Q #' + str(self.counter))
+        self.ids.add_label.text = str(val1) + ' + ' + str(val2)
+        print(str(self.counter))
 
     def validate_ans(self):
         final_input = self.ids.thousands_input.text + self.ids.hundreds_input.text + self.ids.tens_input.text + self.ids.ones_input.text
         if len(final_input) == 0:
             final_input = 0
-        
-        if self.Sum == int(final_input):
-            self.ids.add_label.text = 'Correct!'
-        else:
-            self.ids.add_label.text = 'Wrong Answer, sum is = ' + str(self.Sum)
-        
-        # reload question then move to next question
+
         if DifficultyScreen.difficulty == 'easy':
-            if self.counter < 10:
-                self.counter += 1
-                self.load_easy()
+            self.counter += 1
+            if self.Sum == int(final_input):
+                AdditionScreen.score += 1
+                # move to correct screen
+                self.manager.current = 'correct'
             else:
-                self.manager.current = 'result'
+                # move to wrong screen
+                self.manager.current = 'wrong'
+            
         elif DifficultyScreen.difficulty == 'medium':
-            if self.counter <= 10:
-                self.load_medium()
-                self.counter += 1
+            self.counter += 1
+            if self.Sum == int(final_input):
+                AdditionScreen.score += 1
+                # move to correct screen
+                self.manager.current = 'correct'
             else:
-                pass
+                # move to wrong screen
+                self.manager.current = 'wrong'
         elif DifficultyScreen.difficulty == 'hard':
-            if self.counter <= 10:
-                self.load_hard()
-                self.counter += 1
+            self.counter += 1
+            if self.Sum == int(final_input):
+                AdditionScreen.score += 1
+                # move to correct screen
+                self.manager.current = 'correct'
             else:
-                pass
-        
+                # move to wrong screen
+                self.manager.current = 'wrong'
+    
+    def on_exit(self):
+        self.manager.current = 'menu'
+        self.score = 0
+        self.counter = 1
+    
+    def reset_inputs(self):
+        self.ids.thousands_input.text = ''
+        self.ids.hundreds_input.text = ''
+        self.ids.tens_input.text = ''
+        self.ids.ones_input.text = ''
+    
 
 class SubtractionScreen(Screen):
     def on_pre_enter(self, *args):
@@ -101,7 +128,17 @@ class DivisionScreen(Screen):
         return super().on_pre_enter(*args)
 
 class ResultScreen(Screen):
-    
     def on_pre_enter(self, *args):
-        self.ids.result_label.text = 'Finished!'
+        self.ids.result_label.text = 'Scores is ' + str(AdditionScreen.score) + '/10'
         return super().on_pre_enter(*args)
+
+class CorrectScreen(Screen):
+    pass
+
+class WrongScreen(Screen):
+    def on_pre_enter(self, *args):
+        self.ids.wrong_label.text = 'Wrong Answer, sum is = ' + str(AdditionScreen.Sum)
+        return super().on_pre_enter(*args)
+    
+    def on_next(self):
+        pass
