6e0fb8775a67b7f9d4f53b1cd9f91ad4289b115b
RSarona <saronaraulen@gmail.com>
2022-02-22 18:04:20 +0800

    Updated signup.py and kv to communicate with each other

--- lib/signup.py
+++ lib/signup.py
@@ -18,7 +18,7 @@ class SignupScreen(Screen):
     password = StringProperty('')
     confirm_password = StringProperty('')
 
-    def btn_register_function(self):
+    def on_register(self):
         self.username = self.ids.user_input.text
         self.first_name = self.ids.first_input.text
         self.last_name = self.ids.last_input.text
@@ -33,22 +33,48 @@ class SignupScreen(Screen):
             print ("both fields are empty") # for debugging
             if len(str(self.username)) == 0:
                 print("please enter a username") # print this at the username error label
+                self.ids.user_input.error = True
+                self.ids.user_input.helper_text = 'Please enter a username'
+                self.ids.user_input.focus = True
+                self.ids.user_input.focus = False
             else:
                 print("username is full") # erase username error label text 
+                self.reset_username()
             if len(str(self.password)) == 0:
                 print("please enter a password") # print this at the password error label
+                self.ids.pass_input.error = True
+                self.ids.pass_input.helper_text = 'Please enter a password'
+                self.ids.pass_input.focus = True
+                self.ids.pass_input.focus = False
             else:
                 print ("password is full") # erase error label text
+                self.reset_password()
             if self.password != self.confirm_password:
-                print("Please Confirm your password") # print this at the confirm_password  error label
+                print("Password doesn't match") # print this at the confirm_password  error label
+                self.ids.confirmpass_input.error = True
+                self.ids.confirmpass_input.helper_text = "Password doesn't match"
+                self.ids.confirmpass_input.focus = True
+                self.ids.confirmpass_input.focus = False
+            else:
+                self.reset_confirm_pass()
             if len(str(self.first_name)) == 0:
                 print("please enter your first name")
+                self.ids.first_input.error = True
+                self.ids.first_input.helper_text = 'Please Enter your first name'
+                self.ids.first_input.focus = True
+                self.ids.first_input.focus = False
             else:
                 print("firstname is full") # erase first name error label text
+                self.reset_firstname()
             if  len(str(self.last_name)) == 0:
                 print("please enter your last name ") # print this at the last name  error label
+                self.ids.last_input.error = True
+                self.ids.last_input.helper_text = 'Please Enter your last name'
+                self.ids.last_input.focus = True
+                self.ids.last_input.focus = False
             else:
                 print("lastname is full") # erase last name error label text
+                self.reset_lastname()
         else:  # if all checks pass 
             Username = str(self.username)
             Password = str(self.password)
@@ -60,23 +86,62 @@ class SignupScreen(Screen):
                 print("Register Success")
                 print(params)
                 print("Result is " + str(Registerrequest.result))
-
+                to_login()
+                
             # On Failed message of below's request             
             def failedrequest(self,*args):
                 print ("Failed Request") # show error message 
                 print (params)
                 print ("Result is "+ str(Registerrequest.result))
-
+            
+            def to_login():
+                self.manager.current = 'login'
             
             params =json.dumps({"username":Username,"password":Password, "first_name": First_name, "last_name": Last_name})
             print (params)
             headers= {'Content-type':'application/json','Accept':'text/plain'}
             Registerrequest = UrlRequest('https://uslsthesisapi.herokuapp.com/register', on_success= successrequest,on_failure=failedrequest, req_body=params,req_headers=headers)
 
-    def on_leave(self, *args):
+    def reset_username(self):
+        self.ids.user_input.error = False
+        self.ids.user_input.focus = True
+        self.ids.user_input.focus = False
+
+    def reset_password(self):
+        self.ids.pass_input.error = False
+        self.ids.pass_input.focus = True
+        self.ids.pass_input.focus = False
+    
+    def reset_confirm_pass(self):
+        self.ids.confirmpass_input.error = False
+        self.ids.confirmpass_input.helper_text = ''
+        self.ids.confirmpass_input.focus = True
+        self.ids.confirmpass_input.focus = False
+
+    def reset_firstname(self):
+        self.ids.first_input.error = False
+        self.ids.first_input.helper_text = ''
+        self.ids.first_input.focus = True
+        self.ids.first_input.focus = False
+
+    def reset_lastname(self):
+        self.ids.last_input.error = False
+        self.ids.last_input.helper_text = ''
+        self.ids.last_input.focus = True
+        self.ids.last_input.focus = False
+
+    def reset_all(self):
+        self.reset_username()
+        self.reset_password()
+        self.reset_confirm_pass()
+        self.reset_firstname()
+        self.reset_lastname()
+
+    def on_pre_leave(self, *args):
         self.ids.user_input.text = ''
         self.ids.first_input.text = ''
         self.ids.last_input.text = ''
         self.ids.pass_input.text = ''
         self.ids.confirmpass_input.text = ''
+        self.reset_all()
         return super().on_pre_leave(*args)
