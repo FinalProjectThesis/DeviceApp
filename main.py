96dfbe78b3e7e616dbc20e67130e7da5da789add
Joash Diaz <johnjoashcolangodiaz@gmail.com>
2022-02-24 02:03:06 +0800

    added screen transition

--- main.py
+++ main.py
@@ -17,27 +17,30 @@ from lib.signup import SignupScreen
 from lib.childlist import ChildListScreen
 from lib.menu import MenuScreen, DifficultyScreen
 from lib.op_controller import AdditionScreen, SubtractionScreen, MultiplicationScreen, DivisionScreen, ResultScreen, CorrectScreen, WrongScreen
+from kivy.clock import Clock
 class WindowManager(ScreenManager):
     token = StringProperty('')
     parent_username = StringProperty('')
     student_id = StringProperty('')
 
 class LoadingScreen(Screen):
-    pass
-
-class MainApp(MDApp):
-    def build(self):
-        # file reading 
+    def on_enter(self, *args):
+        Clock.schedule_once(lambda dt: self.execute(),2)
+        return super().on_pre_enter(*args)
+    def execute(self):
         with open('SavedLogin.json') as json_file:
             data = json.load(json_file)
             json_object = json.loads(data)
             if json_object["checkvalue"] == "True":
                 print ("proceed to childlist")
                 print ("extracted value " + json_object["username"])
-                #self.manager.current = 'login' # just a placeholder for now. 
+                self.manager.current = 'login' # just a placeholder for now. 
             else:
                 print("empty, proceed to login")
-                #self.manager.current = 'login'
+                self.manager.current = 'login'
+class MainApp(MDApp):
+    def build(self):
+        # file reading 
         # print("Playing Song")
         # sound = SoundLoader.load('assets/music/general_bg_music.wav')
         # sound.loop = True
@@ -48,6 +51,10 @@ class MainApp(MDApp):
         return Builder.load_file('lib/kv/mainapp.kv')
     
     def on_start(self):
-        self.fps_monitor_start()    # to check speed on rasp
-   
+        self.fps_monitor_start()
+        # to check speed on rasp
+        
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+            
 MainApp().run()
